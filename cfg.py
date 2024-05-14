f9edb14e252092658bf6a8d2bcac703db116be85
1234quan1234 <buianhquan10@gmail.com>
2024-05-14 23:41:05 +0700

    2D_Maze

---
+++ cfg.py
@@ -0,0 +1,8 @@
+SCREENSIZE = (1200, 800)
+BGMPATH = 'resources/audios/bgm.mp3'
+HEROPICPATH = 'resources/images/hero.png'
+FPS = 20
+
+MAZESIZE = (40, 40) # num_rows * num_cols
+BLOCKSIZE = 600//MAZESIZE[0]
+BORDERSIZE = (300, 100)
